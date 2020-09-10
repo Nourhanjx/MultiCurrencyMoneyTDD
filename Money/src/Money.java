d76172c98f66aa52f35fce28c34f421d26aedd16
Nourhan <nourhanjamalx@gmail.com>
2020-09-10 22:42:15 +0200

    Initial tests for Money Class

---
+++ Money/src/Money.java
@@ -0,0 +1,39 @@
+class Money {
+
+
+    protected int amount;
+
+    protected String currency;
+
+    Money(int amount,String currency){
+        this.amount = amount;
+        this.currency=currency;
+    }
+
+
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    static Money dolar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    static Money francs(int amount){
+        return new Money(amount, "CHF");
+    }
+    String currency(){
+        return currency;
+
+    }
+    public String toString(){
+        return amount+""+currency;
+    }
+
+    Money times(int multiplier){
+        return new Money(amount *=  multiplier,currency);
+    }
+
+
+}
---
+++ Money/src/MoneyTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTest {
+
+
+    @Test
+    public void testMultiplication(){
+        Money five= Money.dolar(5);
+        assertEquals( Money.dolar(10), five.times(2));
+        assertEquals(Money.dolar(30), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dolar(5).equals(Money.dolar(5)));
+        assertFalse(Money.dolar(5).equals(Money.dolar(6)));
+        assertTrue(Money.francs(5).equals(Money.francs(5)));
+
+    }
+
+
+    @Test
+    public void testCurrency(){
+        assertEquals("USD",Money.dolar(1).currency());
+        assertEquals("CHF",Money.francs(1).currency());
+    }
+
+    @Test
+    public void testDifferentClassEquality(){
+        assertTrue(new Money(10,"CHF").equals(new Money(10,"CHF")));
+    }
+
+
+}
